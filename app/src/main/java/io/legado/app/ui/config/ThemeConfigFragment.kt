9c40016ebcac690d777b7a74ab3a5105286a8541
Rimchars <rimchars@local>
2026-05-11 20:20:21 +0800

    修复设置乱码并更新紧急补丁发布

--- app/src/main/java/io/legado/app/ui/config/ThemeConfigFragment.kt
+++ app/src/main/java/io/legado/app/ui/config/ThemeConfigFragment.kt
@@ -163,13 +163,27 @@ class ThemeConfigFragment : PreferenceFragment(),
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
         menuInflater.inflate(R.menu.theme_config, menu)
+        updateThemeModeMenu(menu)
         menu.applyTint(requireContext())
+    }
+
+    private fun updateThemeModeMenu(menu: Menu) {
+        menu.findItem(R.id.menu_theme_mode)?.setIcon(themeModeIconRes())
+    }
+
+    private fun themeModeIconRes(): Int {
+        return if (AppConfig.isNightTheme) {
+            R.drawable.ic_daytime
+        } else {
+            R.drawable.ic_brightness
+        }
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
         when (menuItem.itemId) {
             R.id.menu_theme_mode -> {
                 AppConfig.isNightTheme = !AppConfig.isNightTheme
+                menuItem.setIcon(themeModeIconRes())
                 ThemeConfig.applyDayNight(requireContext())
                 return true
             }
